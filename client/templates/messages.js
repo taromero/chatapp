9580b62c44bb07c9e2c338e95e15b400bef7d256
Tomas Romero <canotto90@gmail.com>
2014-10-27 02:16:33 -0300

    Incomplete mentions implementation

--- client/templates/messages.js
+++ client/templates/messages.js
@@ -7,11 +7,30 @@ Template.messages.helpers({
 Template.messages.events({
   'keypress #text_entry': function(evt) {
     if (Helpers.isEnter(evt)) {
-      Messages.insert({
+      var message = {
         author: $('#nick').val(),
         body: $('#text_entry').val(),
         snapshot: Camera.takeSnapshot()
-      })
+      }
+      showNotification(message.author, message.body, message.snapshot)
+      Messages.insert(message)
     }
   }
 })
+
+function showNotification(author, messageBody, icon) {
+  var regex = /@\w+/g
+  var matches = messageBody.match(regex)
+  if (matches && matches.length > 0) {
+    matches = matches.map(function(match) {
+      match.replace('@', '')
+    })
+    var notification = new Notification(author, {
+      body: messageBody,
+      icon: icon
+    })
+    setTimeout(function() {
+      notification.close()
+    }, 5000)
+  }
+}
