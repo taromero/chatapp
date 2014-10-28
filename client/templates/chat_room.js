59243a502a79529f4cd2d7d8718bef6a83a14a14
Tomas Romero <canotto90@gmail.com>
2014-10-28 02:15:17 -0300

    Style fix

--- client/templates/chat_room.js
+++ client/templates/chat_room.js
@@ -1,1 +1,20 @@
 Notification.requestPermission()
+
+Template.chat_room.rendered = function() {
+  Tracker.autorun(showMentions)
+
+  function showMentions() {
+    var user = Session.get('user')
+    var caseInsensitiveNick = new RegExp('^' + user.nick + '$', 'i')
+    var mentionsToUser = Mentions.find({ to: { $in: [caseInsensitiveNick, 'all'] }, from: { $ne: caseInsensitiveNick } })
+    mentionsToUser.forEach(function(msg) {
+      console.log('msg ' , msg);
+      showNotification(msg)
+    })
+    // remove them after they are displayed.
+    // Not user Mentions.remove({ to: user.nick }) because a mention can be created in the meanwhile
+    mentionsToUser.forEach(function(msg) {
+      Mentions.remove(msg._id)
+    })
+  }
+}
--- client/templates/messages.js
+++ client/templates/messages.js
@@ -1,23 +1,3 @@
-Template.messages.rendered = function() {
-  Tracker.autorun(showMentions)
-
-  function showMentions() {
-    var user = Session.get('user')
-    var caseInsensitiveNick = new RegExp('^' + user.nick + '$', 'i')
-    var mentionsToUser = Mentions.find({ to: { $in: [caseInsensitiveNick, 'all'] }, from: { $ne: caseInsensitiveNick } })
-    mentionsToUser.forEach(function(msg) {
-      console.log('msg ' , msg);
-      showNotification(msg)
-    })
-    // remove them after they are displayed.
-    // Not user Mentions.remove({ to: user.nick }) because a mention can be created in the meanwhile
-    mentionsToUser.forEach(function(msg) {
-      Mentions.remove(msg._id)
-    })
-  }
-}
-
-
 Template.messages.helpers({
   messages: function() {
     return Messages.find({}, { sort: { timestamp: -1 } })
