bf12d5ab91c7068d4c4113fbaefb80b507985880
Tomas Romero <canotto90@gmail.com>
2014-10-29 00:08:16 -0300

    Added sepia filter

--- client/templates/status.js
+++ client/templates/status.js
@@ -1,23 +1,9 @@
 Template.status.events({
-  'click #normal': function() {
-    $('video')[0].className = ''
-    Session.set('status.class', '')
-  },
-  'click #call': function() {
-    $('video')[0].className = ''
-    $('video').addClass('grayscale')
-    Session.set('status.class', 'grayscale')
-  },
-  'click #dark': function() {
-    $('video')[0].className = ''
-    $('video').addClass('invert')
-    Session.set('status.class', 'invert')
-  },
-  'click #brb': function() {
-    $('video')[0].className = ''
-    $('video').addClass('blur')
-    Session.set('status.class', 'blur')
-  },
+  'click #normal': setFilter(''),
+  'click #call': setFilter('grayscale'),
+  'click #dark': setFilter('invert'),
+  'click #brb': setFilter('blur'),
+  'click #lunch': setFilter('sepia'),
 
   'click #picSmall': function() {
     Session.set('picHeight', 47)
@@ -32,3 +18,11 @@ Template.status.events({
     Session.set('picWidth', 172)
   }
 })
+
+function setFilter(name) {
+  return function() {
+    $('video')[0].className = ''
+    $('video').addClass(name)
+    Session.set('status.class', name)
+  }
+}
