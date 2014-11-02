c827bfd1c7164323c0ab3b30ade766e7dd5d5710
Tomas Romero <canotto90@gmail.com>
2014-11-02 18:35:28 -0300

    Stop video on leaving room and allow re join

--- client/templates/webRTC/simple_web_rtc.js
+++ client/templates/webRTC/simple_web_rtc.js
@@ -2,22 +2,31 @@ Template.simple_web_rtc.rendered = function() {
   var webrtc = null
   Tracker.autorun(function() {
     if (Session.get('joinCall')) {
-      webrtc = new SimpleWebRTC({
-        // the id/element dom element that will hold "our" video
-        localVideoEl: 'localVideo',
-        // the id/element dom element that will hold remote videos
-        remoteVideosEl: 'remotesVideos',
-        // immediately ask for camera access
-        autoRequestMedia: true
-      });
+      if (webrtc) {
+        webrtc.joinRoom('dafaultRoom')
+        $('#localVideo').show()
+      } else {
+        webrtc = new SimpleWebRTC({
+          // the id/element dom element that will hold "our" video
+          localVideoEl: 'localVideo',
+          // the id/element dom element that will hold remote videos
+          remoteVideosEl: 'remotesVideos',
+          // immediately ask for camera access
+          autoRequestMedia: true
+        });
 
-      // we have to wait until it's ready
-      webrtc.on('readyToCall', function () {
-        // you can name it anything
-        webrtc.joinRoom('your awesome room name')
-      })
+        // we have to wait until it's ready
+        webrtc.on('readyToCall', function () {
+          // you can name it anything
+          webrtc.joinRoom('dafaultRoom')
+          $('#localVideo').show()
+        })
+      }
     } else {
-      webrtc && webrtc.leaveRoom()
+      if (webrtc) {
+        webrtc.leaveRoom()
+        $('#localVideo').hide()
+      }
     }
   })
 }
